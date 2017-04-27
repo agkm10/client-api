dacd09471086900e90f994ec31b7db14111728f8
Ben Biggs <bfletcherbiggs@gmail.com>
2017-04-27 14:51:14 -0500

    parenthesis missing

--- controllers/inputController.js
+++ controllers/inputController.js
@@ -71,13 +71,13 @@ module.exports = {
     updateComps: ( req, res, next ) => {
         let compComplete = {
             completed:req.body.completed
-        }  
+        }
         db( 'components' )
         .where( () => {
             this.where( 'user_id',req.session.passport.user )
             .andWhere( 'compName',req.body.component )
-        }  
-        .update( compComplete,'*' )
+        })  
+        .update( compComplete, '*' )
         .then( results => {
             return res.status( 200 ).json( results )
         })
